96f1abb26fd5e98f70220434d78a432b5e356c5c
Anderson Monte <andersonsmont@gmail.com>
2022-03-06 23:41:17 -0300

    changes

--- src/routes/status.routes.ts
+++ src/routes/status.routes.ts
@@ -2,6 +2,6 @@ import { Request, Response, Router} from "express";
 
 export const statusRoute = Router();
 
-statusRoute.get("/", (req: Request, res: Response) => {
+statusRoute.get("/status", (req: Request, res: Response) => {
 	res.sendStatus(200);
 });
